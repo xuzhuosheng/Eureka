1e6e5c304f7be1571b39201b326ffae393814572
xuzhuosheng <284696920@qq.com>
2019-04-28 15:27:21 +0800

    第一次提交

---
+++ ConfigClientDemo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+/*
+    @Value("${hello}")
+    private String hello;
+
+    public String from(){
+        return  this.hello;
+    }
+*/
+
+
+}
